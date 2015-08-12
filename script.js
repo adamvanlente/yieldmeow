e71f39bd1ee4fd77a1a3a5eb854ce9663b4d98f1
adamvanlente <adamvanlente@gmail.com>
2015-08-11 23:26:23 -0400

    Making repo public.  Cleaning up js/creating namespace.  Adding Connor's cat and giving it preference over the other cats.

--- script.js
+++ script.js
@@ -1,147 +1,128 @@
-function submitSite() {
-  var url = document.getElementById('siteUrl').value;
-  window.location.href = window.location.href + '#' + url;
-  window.location.reload();
-}
+// Yieldmeow namespace.
+var yieldmeow = {
 
-window.onload = function(){
-   if (window.location.hash) {
-      var iframe = document.createElement('iframe');
-      iframe.frameBorder=0;
-      iframe.width="100%";
-      iframe.height="100%";
-      iframe.id="randomid";
-      iframe.setAttribute("src", window.location.hash.replace('#', ''));
-      document.getElementById("meowFrame").appendChild(iframe);
-      document.getElementById("meowFrame").style.display = 'block';
-      document.getElementById("siteUrl").style.display = 'none';
-      document.getElementById("siteButton").style.display = 'none';
-      document.getElementById("heading").style.display = 'none';
-      document.getElementById("subheading").style.display = 'none';
-      document.getElementById("goAgain").style.display = 'block';
-      setInterval(function() {
-        setCats();
-      }, 1000);
-   }
-}
+  // Give a cat a new className.  This will set a target position opposite
+  // the selected cat and send it floating in that direction.
+  setCats: function() {
 
-function setCats() {
-  var cat = getCat();
-  var catImg = document.getElementById(cat);
-  var className;
+    // Get cat img and current class name, then set new class.
+    var cat = yieldmeow.getCat(),
+        className = yieldmeow.getCatClass(cat);
+    $(cat).attr('class', className);
+  },
 
-  if (catImg.className.search('left') !== -1) {
-    className = getRandRightClass();
+  // Get a random cat.
+  getCat: function() {
+    var cats = $('.floatingCat'),
+        idx = Math.floor(Math.random() * cats.length) + 1;
+
+    if (!cats[idx]) {
+      return getCat();
+    } else {
+      return cats[idx];
+    }
+  },
+
+  // Get a class for the cat img, which will be opposite its current class.
+  getCatClass: function(catImg) {
+
+    var randNumber = idx = Math.floor(Math.random() * 4) + 1,
+        classBase;
+
+    if ($(catImg).attr('class').search('left') !== -1)
+      classBase = 'right';
+    if ($(catImg).attr('class').search('right') !== -1)
+      classBase = 'left';
+    if ($(catImg).attr('class').search('bottom') !== -1)
+      classBase = 'top';
+    if ($(catImg).attr('class').search('top') !== -1)
+      classBase = 'bottom';
+
+    // return the random class.
+    return classBase + 'Class_' + randNumber + ' floatingCat';
+  },
+
+  // Get the new site url and yieldmeow the shit out of it.
+  yieldmeowThatMotherfucker: function() {
+    var url = $('#siteUrl').val();
+    window.location.href = window.location.href + '#' + url;
+    window.location.reload();
+  },
+
+  // Add key listener to treat input like a form.
+  addKeyListener: function() {
+    $(document).keyup(function(e) {
+       var key = e.keyCode ? e.keyCode : e.which;
+       if (key == 13 && document.activeElement.id == 'siteUrl') {
+         yieldmeow.yieldmeowThatMotherfucker();
+       }
+    });
+  },
+
+  // Populate the iframe and start floating cats.
+  setIframeAndCats: function() {
+
+    // Get url from user or default.
+    var url = 'http://yieldmo.com';
+    if (window.location.hash) {
+      url = window.location.hash.replace('#', '');
+    }
+
+    // Set up iframe.
+    var iframe = $('<iframe></iframe>')
+      .attr('src', url)
+      .css({
+        'frameBorder': 0,
+        'width': '100%',
+        'height': '100%',
+        'id': 'randomId'
+      });
+    $('#meowFrame').append(iframe);
+
+    // Start setting cats.
+    setInterval(function() {
+      yieldmeow.setCats();
+    }, 500);
+  },
+
+  // Update (show/hide) some elements when yieldmeowing.
+  updateElementsForYieldmeow: function() {
+
+    // Show some elements.
+    var showEls = ['meowFrame', 'goAgain'];
+    $.each(showEls, function(showItm, showID) {
+      $('#' + showID).show();
+    });
+
+    // Hide some elements.
+    var hideEls = ['siteUrl', 'siteButton', 'heading', 'subheading'];
+    $.each(hideEls, function(hideItm, hideID) {
+      $('#' + hideID).hide();
+    });
+  },
+
+  // Reload so user can customize.
+  reload: function() {
+    window.location.href = window.location.href.split('#')[0] + '?new';
+  },
+
+  // Kick off yieldmeow.
+  init: function() {
+    this.addKeyListener();
+
+    // Check for forced stop.  Lets user enter new site.
+    if (window.location.search.search('new') != -1) {
+      window.history.pushState({},"sample","/");
+      return;
+    }
+
+    // Set up the iframe to show a website.
+    this.setIframeAndCats();
+
+    // Update some ui elements.
+    this.updateElementsForYieldmeow();
   }
+};
 
-  if (catImg.className.search('right') !== -1) {
-    className = getRandLeftClass();
-  }
-
-  if (catImg.className.search('bottom') !== -1) {
-    className = getRandTopClass();
-  }
-
-  if (catImg.className.search('top') !== -1) {
-    className = getRandBottomClass();
-  }
-  catImg.className = className;
-}
-
-function getCat() {
-
-  var cats = [
-    'cat_1',
-    'cat_2',
-    'cat_3',
-    'cat_4',
-    'cat_5',
-    'cat_6',
-    'cat_7',
-    'cat_8',
-    'cat_9',
-    'cat_10',
-    'cat_11',
-    'cat_12',
-    'cat_13',
-    'cat_14',
-    'cat_15',
-    'cat_16',
-    'cat_17',
-    'cat_18',
-    'cat_19',
-    'cat_20'
-  ];
-  var idx = Math.floor(Math.random() * cats.length) + 1;
-  if (!cats[idx]) {
-    return getCat();
-  } else {
-    return cats[idx];
-  }
-
-}
-
-function getRandLeftClass() {
-  var classes = [
-    'leftClass_1',
-    'leftClass_2',
-    'leftClass_3',
-    'leftClass_4'
-  ];
-  var idx = Math.floor(Math.random() * classes.length) + 1;
-  if (!classes[idx]) {
-    return getRandLeftClass();
-  } else {
-    return classes[idx] + ' slowMovement'
-  }
-}
-
-
-function getRandRightClass() {
-  var classes = [
-    'rightClass_1',
-    'rightClass_2',
-    'rightClass_3',
-    'rightClass_4'
-  ];
-  var idx = Math.floor(Math.random() * (classes.length - 1)) + 1;
-  if (!classes[idx]) {
-    return getRandRightClass();
-  } else {
-    return classes[idx] + ' slowMovement'
-  }
-}
-
-function getRandBottomClass() {
-  var classes = [
-    'bottomClass_1',
-    'bottomClass_2',
-    'bottomClass_3',
-    'bottomClass_4'
-  ];
-  var idx = Math.floor(Math.random() * (classes.length - 1)) + 1;
-  if (!classes[idx]) {
-    return getRandBottomClass();
-  } else {
-    return classes[idx] + ' slowMovement'
-  }
-}
-
-function getRandTopClass() {
-  var classes = [
-    'topClass_1',
-    'topClass_2',
-    'topClass_3',
-    'topClass_4',
-  ];
-  var idx = Math.floor(Math.random() * (classes.length - 1)) + 1;
-  if (!classes[idx]) {
-    return getRandTopClass();
-  } else {
-    return classes[idx] + ' slowMovement'
-  }
-}
-
-function reloadYM() {
-  window.location.href = window.location.href.split('#')[0];
-}
+// Get it going.
+yieldmeow.init();
